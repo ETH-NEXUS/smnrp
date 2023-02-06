260b9b12b21e1ce328bf9daf3697bcf937a3cddb
dameyerdave <dameyerdave@gmail.com>
2023-02-06 16:41:28 +0100

    [FIX] stop reloader on platforms w/o support for inotifywait

--- reloader.sh
+++ reloader.sh
@@ -6,12 +6,19 @@ mkdir -p /etc/letsencrypt/live /etc/nginx/conf.d
 while true
 do
   inotifywait -r --exclude .swp -e create -e modify -e delete -e move /etc/letsencrypt/live
+  if [ $? -ne 0 ]; then
+    echo "### inotifywait exited with status != 1 ($?)."
+    echo "### reloader will exit"
+    exit 1
+  else
+    echo "### Change detected in /etc/letsencrypt/live ($?)."
+  fi
   nginx -t
   if [ $? -eq 0 ]; then
-    echo "Detected Nginx change"
-    echo "Executing: nginx -s reload"
+    echo "### Detected Nginx change"
+    echo "### Executing: nginx -s reload"
     nginx -s reload
   else
-    echo "Errors detected in nginx config, not reloading."
+    echo "### Errors detected in nginx config, not reloading."
   fi
 done
