20f2347e2b255424259440959c3f6594c25f6e0b
dameyerdave <dameyerdave@gmail.com>
2024-06-03 09:41:44 +0200

    [FEAT] Runs renewer only every 12h

--- renewer.sh
+++ renewer.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 
+ts_file='/etc/letsencrypt/last_renew.ts'
+
+readarray -d '|' -t vhosts < <(printf '%s' "${SMNRP_DOMAINS}")
+readarray -d '|' -t vhost_own_cert < <(printf '%s' "${SMNRP_OWN_CERT}")
+readarray -d '|' -t vhost_self_signed < <(printf '%s' "${SMNRP_SELF_SIGNED}")
 while true
-do 
-  if [ "${SMNRP_SELF_SIGNED}" != 'true' ] && [ "${SMNRP_OWN_CERT}" != 'true' ]; then
-    certbot renew 
+do
+  renew=0
+  for i in "${!vhosts[@]}"
+  do
+    # Check if there's at least one vhost with a let's encrypt certificate
+    if [[ "${vhost_own_cert[i]}" != 'true' ]] && [[ "${vhost_self_signed[i]}" != 'true' ]]; then
+      renew=1
+      break
+    fi
+  done
+  if [ $renew -eq 1 ]; then
+    if [ ! -e ${ts_file} ]; then
+      echo "$(date +%s)" > ${ts_file}
+      last=0
+    else
+      last=$(cat ${ts_file})
+    fi
+    now=$(date +%s)
+    diff=$(expr $now - $last)
+    # we only renew to if last renew was ~12h ago
+    if [ $diff -gt 43190 ]; then
+      echo "Trying to renew certificate..."
+      certbot renew
+      echo "$(date +%s)" > ${ts_file}
+    else
+      echo "Still waiting for renewal ${diff}..."
+    fi
   fi
+  # we wait 12h until next renew
   sleep 12h
 done
